05e4261a974f6983c6a6f9dcd189e328119823d0
Bora Karaman <borakaraman@Boras-MacBook-Pro.local>
2026-04-02 03:44:37 +0300

    initial commit

---
+++ connector.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+
+# Test connection
+with engine.connect() as conn:
+    print("Connected successfully")
